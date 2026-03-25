25a6b1f08a43418a83c1b38687c0a6abc68ca54b
Sara <sararuizjuarez@gmail.com>
2026-03-25 15:58:13 +0100

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = 3017
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hola Mundo'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+})
